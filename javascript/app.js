efbf889bebfdd18dd9c8aeea6d19163d446acf98
fatihyalcn <fatihyalcn9332@gmail.com>
2022-01-20 22:01:31 +0100

    js-data-types-added

---
+++ javascript/app.js
@@ -0,0 +1,50 @@
+/* console.log("I'm from file");
+
+var x = 7;
+var y = 5;
+var y = x;
+
+console.log(x);
+console.log({x});
+console.log({y}); */
+
+
+
+/* var alert = "Merhaba"
+console.log(alert);
+ */
+/* alert('Hey')
+ */
+
+/* var a = "c";
+console.log(a);
+
+ */
+const aa = 5;
+console.log(aa);
+
+console.log(typeof 20);
+console.log(typeof Infinity);
+
+var car = "engine"
+console.log(typeof car);
+
+var user = 'Fatih'
+var v = `merhaba ${user}`
+console.log(v);
+
+console.log(Boolean(" "));
+
+console.log(Boolean(3 == 5));
+
+console.log(Boolean(3 < 5));
+
+console.log(Boolean(0));
+
+console.log(Boolean("0"));
+
+console.log(Boolean(null));
+
+console.log(2 === '2');
+
+console.log(2 == '2.0');
