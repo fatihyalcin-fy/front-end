1f8021b606759f612a5d2f82fbaa2283ca511181
fatihyalcn <fatihyalcn9332@gmail.com>
2022-12-17 09:28:06 +0100

    update

--- javascript/app.js
+++ javascript/app.js
@@ -1,89 +1,84 @@
-/* console.log("I'm from file");
+// console.log("I'm from file");
 
-var x = 7;
-var y = 5;
-var y = x;
+// var x = 7;
+// var y = 5;
+// var y = x;
 
-console.log(x);
-console.log({x});
-console.log({y}); */
+// console.log(x);
+// console.log({ x });
+// console.log({ y });
 
+// /* var alert = "Merhaba"
+// console.log(alert);
+//  */
+// /* alert('Hey')
+//  */
 
+// /* var a = "c";
+// console.log(a);
 
-/* var alert = "Merhaba"
-console.log(alert);
- */
-/* alert('Hey')
- */
+//  */
+// /* const aa = 5;
+// console.log(aa);
 
-/* var a = "c";
-console.log(a);
+// console.log(typeof 20);
+// console.log(typeof Infinity);
 
- */
-/* const aa = 5;
-console.log(aa);
+// var car = "engine"
+// console.log(typeof car);
 
-console.log(typeof 20);
-console.log(typeof Infinity);
+// var user = 'Fatih'
+// var v = `merhaba ${user}`
+// console.log(v);
 
-var car = "engine"
-console.log(typeof car);
+// console.log(Boolean(" "));
 
-var user = 'Fatih'
-var v = `merhaba ${user}`
-console.log(v);
+// console.log(Boolean(3 == 5));
 
-console.log(Boolean(" "));
+// console.log(Boolean(3 < 5));
 
-console.log(Boolean(3 == 5));
+// console.log(Boolean(0));
 
-console.log(Boolean(3 < 5));
+// console.log(Boolean("0"));
 
-console.log(Boolean(0));
+// console.log(Boolean(null));
 
-console.log(Boolean("0"));
+// console.log(2 === '2');
 
-console.log(Boolean(null));
+// console.log(2 == '2.0'); */
 
-console.log(2 === '2');
+// var x = 0.1 + 0.2;
+// console.log(x);
+// console.log(+x.toFixed(5));
 
-console.log(2 == '2.0'); */
+// console.log(15 + 25);
+// console.log(015 + 025);
 
-var x = 0.1 + 0.2
-console.log(x);
-console.log(+x.toFixed(5));
+// console.log(0234 + 012);
 
+// var z = 22;
+// var t = "2";
+// console.log(z - t);
 
-console.log(15+25);
-console.log(015+025);
+// /* var input = prompt("Enter user name:")
+// console.log(typeof input);
 
-console.log(0234+012);
+// var input = +prompt("Enter age:")
+// console.log(typeof input); */
 
-var z = 22;
-var t = '2';
-console.log(z - t);
+// /* var c = '3';
+// console.log(-c, typeof -c);
 
-/* var input = prompt("Enter user name:")
-console.log(typeof input);
+// var a = 'a';
+// var b = 2;
 
-var input = +prompt("Enter age:")
-console.log(typeof input); */
+// console.log(typeof a !== typeof b); */
 
-/* var c = '3';
-console.log(-c, typeof -c);
+// /* console.log('FullStack' > 'Fuo'); */
 
+// let score = 30;
+// let grade = score >= 50;
 
-var a = 'a';
-var b = 2;
-
-console.log(typeof a !== typeof b); */
-
-/* console.log('FullStack' > 'Fuo'); */
-
-let score = 30;
-let grade = score >= 50
-
-if (grade) {
-    console.log(`You passed the exam with the score of ${score}`);
-    
-}else console.log(`You failed with the score of ${score}`);
+// if (grade) {
+//   console.log(`You passed the exam with the score of ${score}`);
+// } else console.log(`You failed with the score of ${score}`);
